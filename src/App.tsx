761c1a0148a1daebfe10cd8543f02e4d37d9724d
Vikas Thapa <142878082+Thapavikas@users.noreply.github.com>
2025-03-03 16:32:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoList from './components/TodoList'
+
+const App = () => {
+  return (
+    <div className=' bg-black h-screen text-white ' >
+      <TodoList/>
+    </div>
+  )
+}
+
+export default App
